bd56e4d3f479061d7b527138b5b89a97851b6dbc
Kristiana Blake <kristiana.blake@gmail.com>
2021-06-22 13:54:11 -0500

    added IdList component

---
+++ fetch-rewards/src/components/IdList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IdList = (props) => {
+
+    return (
+        <td>
+             <h5>{props.listId}</h5>
+        </td>
+    )
+
+  
+}
+
+export default IdList; 
